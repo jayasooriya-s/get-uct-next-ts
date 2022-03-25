7c1bc300d4fdfaf19c61372c0c36eb4791db5646
jayasooriya <sjsooriya96@gmail.com>
2022-03-25 20:08:39 +0530

    for testing space added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 import styles from "../styles/Home.module.css";
 
@@ -8,6 +9,8 @@ const Home: NextPage = () => {
   return (
     <div>
       <NavBar></NavBar>
+      <div className={styles.height}></div>
+      <Footer></Footer>
     </div>
   );
 };
