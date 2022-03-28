708c37dc161c3c83b8edd2cd055338f5ebb98e5a
jayasooriya <sjsooriya96@gmail.com>
2022-03-28 22:20:55 +0530

    custom component created

---
+++ components/ContainerWithLogo/ContainerWithIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./ContainerWithIcon.module.css";
+import { BsGraphUp } from "react-icons/bs";
+
+type props = {
+  icon?: any;
+  title?: String;
+  description?: any;
+};
+
+export default function ContainerWithIcon(props: props) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.iconContainer}>
+        {props.icon ?? <BsGraphUp className={styles.icon} />}
+      </div>
+      <div>
+        <div className={styles.headLine}>{props.title}</div>
+        <div className={styles.para}>{props.description}</div>
+      </div>
+    </div>
+  );
+}
