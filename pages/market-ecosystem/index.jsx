e393c298c1049f587602b24bcdaa0c84c65096d4
jayasooriya <sjsooriya96@gmail.com>
2022-03-30 21:49:58 +0530

    bug fixed

--- pages/market-ecosystem/index.jsx
+++ pages/market-ecosystem/index.jsx
@@ -1,18 +1,6 @@
 import React from "react";
-import Image from "next/image";
-import imageWeb from "../../public/images/market-ecosystem/market-ecosystem-web.jpg";
 import styles from "./MarketEcosystem.module.css";
 
 export default function MarketEcosystem() {
-  return (
-    <div className={styles.imageContainer}>
-      {/* <Image
-        src={imageWeb.src}
-        alt={""}
-        layout="fill"
-        height={5745}
-        width={1200}
-      ></Image> */}
-    </div>
-  );
+  return <div className={styles.imageContainer}></div>;
 }
