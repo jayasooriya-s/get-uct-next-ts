bedc34e85459019c9f7061333ba91c8095d8e095
jayasooriya <sjsooriya96@gmail.com>
2022-04-03 22:52:01 +0530

    porfolio list created

---
+++ repository/PortfolioData.ts
@@ -0,0 +1,49 @@
+import { Portfolio } from "../models/PortfolioModel";
+
+export const portfolioList: Portfolio[] = [
+  new Portfolio({
+    title: "Food Delivery Application",
+    icon: "https://getuct.com/images/food-delivery-app-icon-black.jpg",
+    imageWeb: "https://getuct.com/images/food.jpg",
+  }),
+  new Portfolio({
+    icon: "https://getuct.com/images/hotel-room-booking-app-black.jpg",
+    title: "Booking Service Application",
+    imageWeb: "https://getuct.com/images/Hotel.jpg",
+  }),
+  new Portfolio({
+    title: "Queue Management System",
+    icon: "https://getuct.com/images/que-management-icon-black.jpg",
+    imageWeb: "https://getuct.com/images/Queue.jpg",
+  }),
+  new Portfolio({
+    title: "Truck On Demand",
+    icon: "https://getuct.com/images/truck-app-icon.jpg",
+    imageWeb: "https://getuct.com/images/travel.jpg",
+  }),
+  new Portfolio({
+    title: "Online Doctor Consulting System",
+    icon: "https://getuct.com/images/docondemand-icon-black.jpg",
+    imageWeb: "https://getuct.com/images/DocOnDemand.jpg",
+  }),
+  new Portfolio({
+    title: "School Management System",
+    icon: "https://getuct.com/images/school-management-brochure-black.jpg",
+    imageWeb: "https://getuct.com/images/School.jpg",
+  }),
+  new Portfolio({
+    title: "Bike Taxi Application",
+    icon: "https://getuct.com/images/dropped-icon-black.jpg",
+    imageWeb: "https://getuct.com/images/Dropped.jpg",
+  }),
+  new Portfolio({
+    title: "Messenger Application",
+    icon: "https://getuct.com/images/messege-brochure-icon-black.jpg",
+    imageWeb: "	https://getuct.com/images/Messenger.jpg",
+  }),
+  new Portfolio({
+    title: "Online Marketplace System",
+    icon: "https://getuct.com/images/ecommerce-icon-black.jpg",
+    imageWeb: "	https://getuct.com/images/E-Commerce.jpg",
+  }),
+];
