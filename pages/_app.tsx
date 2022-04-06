49c460aca1ffbd39b2958cbf1075584aff05070f
jayasooriya <sjsooriya96@gmail.com>
2022-04-06 08:52:35 +0530

    meta description added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/index";
-import { ReactElement } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Layout page={<Component {...pageProps} />}></Layout>;
+  return (
+    <>
+      <Head>
+        <title>Universal Code technology</title>
+        <meta
+          name="description"
+          content="Our marketplace ecosystem enables natural pull on both the consumer and supplier side of the market. Both consumers and suppliers should sense an increasing importance of your online marketplace in the world."
+        />
+      </Head>
+      <Layout page={<Component {...pageProps} />}></Layout>{" "}
+    </>
+  );
 }
 
 export default MyApp;
