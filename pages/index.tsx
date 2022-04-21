b4f52e28b8cbc6a51b2ac4d6c8910231b42712cf
jayasooriya <sjsooriya96@gmail.com>
2022-04-21 21:33:17 +0530

    home screen api integrated

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import { RiHandCoinLine, RiSuitcaseLine } from "react-icons/ri";
 import { GiMoneyStack } from "react-icons/gi";
 import { BiNotepad } from "react-icons/bi";
 import homeBanner from "../public/home/home-banner-top.avif";
-import mission from "../public/home/mission.avif";
 import consult from "../public/home/consult.avif";
 import strategy from "../public/home/strategy.avif";
 import technology from "../public/home/technology.avif";
@@ -17,70 +16,96 @@ import {
   ImageWithBottomIconTitle,
   TopBanner,
 } from "../components";
+import { useEffect, useState } from "react";
+import { domain } from "../config";
 
 const Home: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [apiData, setApiData] = useState<IHomeScreen>({
+    topBanner: { bigTitle: "" },
+  });
+  useEffect(() => {
+    fetchPortfolio();
+  }, []);
+
+  const fetchPortfolio = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `${domain}/api/home-screens?populate[topBanner][populate]=*&populate[firstImageAndDescription][populate]=*&populate[secondImageAndDescription][populate]=*`
+      );
+      const dataList = await response.json();
+      setApiData({
+        topBanner: {
+          smallTitle: dataList.data[0].attributes.topBanner.smallTitle ?? "",
+          bigTitle: dataList.data[0].attributes.topBanner.bigTitle,
+          image: `${domain}${dataList.data[0].attributes.topBanner.image.data.attributes.formats.large.url}`,
+        },
+        imageAndDescriptionFirst: {
+          title: dataList.data[0].attributes.firstImageAndDescription.title,
+          description:
+            dataList.data[0].attributes.firstImageAndDescription.description,
+          isImageFirst:
+            dataList.data[0].attributes.firstImageAndDescription.isImageFirst,
+          image: `${domain}${dataList.data[0].attributes.firstImageAndDescription.image.data.attributes.formats.large.url}`,
+        },
+        imageAndDescriptionSecond: {
+          title: dataList.data[0].attributes.secondImageAndDescription.title,
+          description:
+            dataList.data[0].attributes.secondImageAndDescription.description,
+          isImageFirst:
+            dataList.data[0].attributes.secondImageAndDescription.isImageFirst,
+          image: `${domain}${dataList.data[0].attributes.secondImageAndDescription.image.data.attributes.formats.large.url}`,
+        },
+      });
+
+      setIsLoading(false);
+    } catch (e) {
+      console.log(e);
+      setIsLoading(true);
+    }
+  };
   return (
     <div>
       <TopBanner
-        backgroundImage={homeBanner.src}
-        title="MARKETPLACE SPECIALISTS"
-        titleBig="MOMENTUM IS INCREASING"
+        backgroundImage={apiData.topBanner!.image ?? homeBanner.src}
+        title={apiData.topBanner!.smallTitle ?? "MARKETPLACE SPECIALISTS "}
+        titleBig={apiData.topBanner!.bigTitle ?? "MOMENTUM IS INCREASING"}
       />
       <div className={styles.imageAndRow}>
         <ImageIconRow
-          title={"OUR MISSION"}
+          title={apiData.imageAndDescriptionFirst?.title ?? "OUR MISSION"}
           icon={<BsGraphUp />}
-          imgUrl={"https://getuct.com/wp-content/uploads/2021/06/5-1.png"}
+          isImageFirst={apiData.imageAndDescriptionFirst?.isImageFirst}
+          imgUrl={
+            apiData.imageAndDescriptionFirst?.image ??
+            "https://getuct.com/wp-content/uploads/2021/06/5-1.png"
+          }
           description={
-            <>
-              <p>
-                Our marketplace ecosystem enables natural pull on both the
-                consumer and supplier side of the market. Both consumers and
-                suppliers should sense an increasing importance of your online
-                marketplace in the world.
-              </p>
-              <p>
-                It should be positioned and designed to acquire the incremental
-                consumer as well as the incremental supplier at ease with the
-                flow.
-              </p>
-              <p>
-                We ensure that our clients are enabled with top of the line
-                technology infrastructure along with the market ecosystem
-                operational and call center infrastructures in a dedicated model
-                leveraging our collective economies of scale.
-              </p>
-            </>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: apiData.imageAndDescriptionFirst?.description ?? "",
+              }}
+            ></div>
           }
         />
       </div>
       <div className={styles.imageAndRow}>
         <ImageIconRow
-          title={"CONSULTING, DEVELOPMENT AND SUPPORT EXPERTISE"}
-          icon={<RiHandCoinLine />}
-          imgUrl={consult.src}
-          description={
-            <>
-              <p>
-                Our marketplace ecosystem enables natural pull on both the
-                consumer and supplier side of the market. Both consumers and
-                suppliers should sense an increasing importance of your online
-                marketplace in the world.
-              </p>
-              <p>
-                It should be positioned and designed to acquire the incremental
-                consumer as well as the incremental supplier at ease with the
-                flow.
-              </p>
-              <p>
-                We ensure that our clients are enabled with top of the line
-                technology infrastructure along with the market ecosystem
-                operational and call center infrastructures in a dedicated model
-                leveraging our collective economies of scale.
-              </p>
-            </>
+          title={
+            apiData.imageAndDescriptionSecond?.title ??
+            "CONSULTING, DEVELOPMENT AND SUPPORT EXPERTISE"
           }
-          isImageFirst={false}
+          icon={<RiHandCoinLine />}
+          imgUrl={apiData.imageAndDescriptionSecond?.image ?? consult.src}
+          description={
+            <div
+              dangerouslySetInnerHTML={{
+                __html: apiData.imageAndDescriptionSecond?.description ?? "",
+              }}
+            ></div>
+          }
+          isImageFirst={apiData.imageAndDescriptionSecond?.isImageFirst}
         />
       </div>
 
@@ -126,3 +151,21 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+interface IHomeScreen {
+  topBanner?: ITopBanner;
+  imageAndDescriptionFirst?: IImageAndDescription;
+  imageAndDescriptionSecond?: IImageAndDescription;
+}
+
+interface ITopBanner {
+  smallTitle?: string;
+  bigTitle?: string;
+  image?: string;
+}
+interface IImageAndDescription {
+  title: string;
+  description: string;
+  image: string;
+  isImageFirst: Boolean;
+}
