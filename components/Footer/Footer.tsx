697665071c77987252dd080bcb9f3f97799e4dbf
jayasooriya <sjsooriya96@gmail.com>
2022-04-21 22:10:57 +0530

    footer api completed

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Footer.module.css";
 import { FaHome } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import IconAndText from "../IconAndText/IconAndText";
 import logo from "/public/logo.png";
 import bg from "../../public/footer.avif";
+import { domain } from "../../config";
 
 export default function Footer() {
+  const [footerItem, setFooterItems] = useState<IFooterItems>();
+  const [logoData, setLogo] = useState<string>();
+  useEffect(() => {
+    fetchPortfolio();
+  }, []);
+
+  function backToTop() {
+    window.scrollTo(0, 0);
+  }
+  const fetchPortfolio = async () => {
+    try {
+      const response = await fetch(
+        `${domain}/api/footer?populate=backgroundImage`
+      );
+      const jsonData = await response.json();
+      setFooterItems({
+        address: jsonData.data.attributes.address,
+        contactEmail: jsonData.data.attributes.contactEmail,
+        locations: jsonData.data.attributes.locations,
+        bg: `${domain}${jsonData.data.attributes.backgroundImage.data.attributes.formats.large.url}`,
+      });
+      const logoResponse = await fetch(`${domain}/api/logo?populate=logo`);
+      const jsonDataLogo = await response.json();
+      setLogo(
+        `${domain}${jsonDataLogo.data.attributes.logo.data.attributes.url}`
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <div
       className={styles.container}
       style={{
-        backgroundImage: `url(${bg.src})`,
+        backgroundImage: `url(${footerItem?.bg ?? bg.src})`,
         backgroundRepeat: "no-repeat",
       }}
     >
@@ -22,20 +53,35 @@ export default function Footer() {
               <div className={styles.info}>
                 <IconAndText
                   icon={<FaHome />}
-                  text="724 Tamani Arts, Business Bay, Dubai"
+                  text={
+                    footerItem?.address ??
+                    "724 Tamani Arts, Business Bay, Dubai"
+                  }
                 />
-                <IconAndText icon={<MdEmail />} text="contact@getuct.com" />
+                <IconAndText
+                  icon={<MdEmail />}
+                  text={footerItem?.contactEmail ?? "contact@getuct.com"}
+                />
               </div>
             </div>
           </div>
           <div className={styles.container_logo}>
-            <img src={logo.src} alt="" className={styles.logo} />
+            <img src={logoData ?? logo.src} alt="" className={styles.logo} />
           </div>
         </div>
 
         <div className={styles.divider}></div>
-        <div className={styles.location}>LONDON DUBAI MUMBAI CHENNAI</div>
+        <div className={styles.location}>
+          {footerItem?.locations ?? "LONDON DUBAI MUMBAI CHENNAI"}
+        </div>
       </div>
     </div>
   );
+}
+
+interface IFooterItems {
+  address: string;
+  locations: string;
+  contactEmail: string;
+  bg: string;
 }
