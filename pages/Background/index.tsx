4ccac91b6a8fe737479238eb6941a65959f88507
jayasooriya <sjsooriya96@gmail.com>
2022-04-27 22:48:55 +0530

    background screen api integrated

--- pages/Background/index.tsx
+++ pages/Background/index.tsx
@@ -1,50 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RiSoundcloudLine } from "react-icons/ri";
 import { ImageIconRow, TopBanner } from "../../components";
+import { domain } from "../../config";
 import styles from "./Background.module.css";
 
 export default function Background() {
+  const [apiData, setApiData] = useState<IBackGroundScreen>({
+    topBanner: { bigTitle: "" },
+  });
+  useEffect(() => {
+    fetchPortfolio();
+  }, []);
+
+  const fetchPortfolio = async () => {
+    try {
+      const response = await fetch(
+        `${domain}/api/back-ground-screen?populate[topBanner][populate]=*&populate[imageAndDescription][populate]=*`
+      );
+      let _apiData = await response.json();
+      setApiData({
+        topBanner: {
+          bigTitle: _apiData.data.attributes.topBanner.bigTitle,
+          smallTitle: _apiData.data.attributes.topBanner.smallTitle,
+          image: `${domain}${_apiData.data.attributes.topBanner.image.data.attributes.url}`,
+        },
+        imageAndDescription: {
+          title: _apiData.data.attributes.imageAndDescription.title,
+          description: _apiData.data.attributes.imageAndDescription.description,
+          isImageFirst: _apiData.data.attributes.imageAndDescription.title,
+          image: `${domain}${_apiData.data.attributes.imageAndDescription.image.data.attributes.url}`,
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <div>
-      <TopBanner backgroundImage="https://getuct.com/wp-content/uploads/2021/06/pexels-photo-830891-scaled.jpeg" />
+      <TopBanner
+        backgroundImage={
+          apiData.topBanner?.image ??
+          "https://getuct.com/wp-content/uploads/2021/06/pexels-photo-830891-scaled.jpeg"
+        }
+        title={apiData.topBanner?.smallTitle}
+        titleBig={apiData.topBanner?.bigTitle}
+      />
       <div className={styles.imageAndRow}>
         <ImageIconRow
           imgUrl={
+            apiData.imageAndDescription?.image ??
             "https://getuct.com/wp-content/uploads/2021/06/marketplace2.png"
           }
+          title={apiData.imageAndDescription?.title}
           icon={<RiSoundcloudLine />}
           description={
-            <>
-              <p>
-                Have built marketplace that handles millions of transactions
-                every month running into multi million dollars of values
-              </p>
-              <p>
-                Have built the necessary technology infrastructure that handles
-                transactions of the above scale
-              </p>
-              <p>
-                Have built support infrastructure that facilitates operations
-                enabling such scale of volumes in transactions & currency
-              </p>
-              <p>
-                Have manoeuvered the necessary challenges that one encounters on
-                the growth path of such scales and established processes,
-                technology & server upgrades to evolve further.
-              </p>
-              <p>
-                Last but not least, having our marketplaces valued at 100
-                million dollars worth in the eyes of valuation perspectives of
-                investors have been built into our strategy & technology.
-              </p>
-              <p>
-                Hence we help conceive, consult, build, operate and deliver
-                digital marketplaces that achieve exponential growth.
-              </p>
-            </>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: apiData.imageAndDescription?.description ?? "",
+              }}
+            ></div>
           }
+          isImageFirst={apiData.imageAndDescription?.isImageFirst}
         />
       </div>
     </div>
   );
+}
+
+interface IBackGroundScreen {
+  topBanner?: ITopBanner;
+  imageAndDescription?: IImageAndDescription;
+}
+
+interface ITopBanner {
+  smallTitle?: string;
+  bigTitle?: string;
+  image?: string;
+}
+interface IImageAndDescription {
+  title: string;
+  description: string;
+  image: string;
+  isImageFirst: Boolean;
 }
